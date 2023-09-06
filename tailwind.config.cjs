1c7de29257930ba0babd174e6e806c454232d4b6
Ida Franzén Karlsson <frazzoot@gmail.com>
2023-09-06 18:34:08 +0200

    Index carousel and new logo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {
       fontFamily: {
-        "jost": ["Jost"],
-        "francoisOne": ["FrancoisOne"]
+        jost: ["Jost"],
+        francoisOne: ["FrancoisOne"],
       },
       colors: {
         "grey-dark": "#2E2E2E",
@@ -14,5 +17,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 };
